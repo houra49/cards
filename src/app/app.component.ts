8c7f950e1160f0f3beb0319231068637535f9dd4
Houra49 <email@youremail.com>
2022-03-15 12:01:32 +0100

    Cards Project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,24 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'cards';
+posts=[
+{
+  title:'Neat Tree',
+  imageUrl:'assets/tree.jpg',
+  username:'@nature',
+  content:'I saw this neat today!!'
+},
+{
+title:'Snowy mountain',
+imageUrl:'assets/mountain.jpg',
+username:'@mountainlover',
+content:'Here is a picture of a snowy mountain.'
+},
+{
+title:'Mountain biking',
+imageUrl:'assets/biking.jpg',
+username:'@mauntainbiking',
+content:'I did some biking today'
+}
+]
 }
